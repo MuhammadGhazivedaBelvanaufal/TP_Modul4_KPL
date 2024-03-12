a5cb0a68607baa5735b22972496a1202b6c721b4
belvanaufal <qhikizaalicia@gmail.com>
2024-03-12 13:31:44 +0700

    Menambahkan implementasi dengan Table-Driven

---
+++ KodePos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302220011
+{
+    public class KodePos
+    {
+        public static Dictionary<string, int> tabelKodePos = new Dictionary<string, int>
+    {
+        {"Batununggal", 40266},
+        {"Kujangsari", 40287},
+        {"Mengger", 40267},
+        {"Wates", 40256},
+        {"Cijaura", 40287},
+        {"Jatisari", 40286},
+        {"Margasari", 40286},
+        {"Sekejati", 40286},
+        {"Kebonwaru", 40272},
+        {"Maleer", 40274},
+        {"Samoja", 40273}
+    };
+
+        public int getKodePos(string kelurahan)
+        {
+            int kodePos;
+            if (tabelKodePos.ContainsKey(kelurahan))
+            {
+                kodePos = tabelKodePos[kelurahan];
+            }
+            else
+            {
+                Console.Write($"Kode pos untuk kelurahan '{kelurahan}' tidak ditemukan. Masukkan kode pos: ");
+                while (!int.TryParse(Console.ReadLine(), out kodePos))
+                {
+                    Console.Write("Input tidak valid. Masukkan angka untuk kode pos: ");
+                }
+                tabelKodePos[kelurahan] = kodePos;
+            }
+            return kodePos;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul4_1302220011;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        KodePos kodePos = new KodePos();
+        Console.Write("Masukkan nama kelurahan: ");
+        string kelurahan = Console.ReadLine();
+        int kodePosKelurahan = kodePos.getKodePos(kelurahan);
+        Console.WriteLine($"Kode pos untuk kelurahan {kelurahan} adalah {kodePosKelurahan}");
+
+        // Memanggil seluruh isi tabel kode pos
+        Console.WriteLine("\nDaftar Kode Pos:");
+        foreach (var item in KodePos.tabelKodePos)
+        {
+            Console.WriteLine("{0}: {1}", item.Key, item.Value);
+        }
+    }
+}
